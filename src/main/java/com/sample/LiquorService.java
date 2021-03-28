30764df778ece36edb70e836bfd8df260c429179
Jeanette Perez <jeanette.marie.perez@gmail.com>
2021-03-28 18:10:11 -0500

    testing code

--- src/main/java/com/sample/LiquorService.java
+++ src/main/java/com/sample/LiquorService.java
@@ -9,13 +9,13 @@ public class LiquorService {
 
     public List<String> getAvailableBrands(LiquorType type) {
         List<String> brands = new ArrayList<String>();
-        if(type.equals(LiquorType.WINE)){
+        if (type.equals(LiquorType.WINE)) {
             brands.add("Orin Swift");
             brands.add("Caymus Vineyards");
-        } else if (type.equals(LiquorType.WHISKEY)){
+        } else if (type.equals(LiquorType.WHISKEY)) {
             brands.add("Rebecca Creek");
             brands.add("Bookers");
-        } else if (type.equals(LiquorType.BEER)){
+        } else if (type.equals(LiquorType.BEER)) {
             brands.add("Freetail");
             brands.add("Ballast Point");
         } else {
@@ -23,4 +23,11 @@ public class LiquorService {
         }
         return brands;
     }
+
+
+//    public static void main(String[] args) {
+//        LiquorService test = new LiquorService();
+//        var tester = test.getAvailableBrands(LiquorType.WHISKEY);
+//        System.out.println(tester);
+//    }
 }
