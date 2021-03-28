0da75cde31e5d6c2bc4f36e7cd75f85fd5535b69
Jeanette Perez <jeanette.marie.perez@gmail.com>
2021-03-28 18:04:30 -0500

    Class that stores avaliable brands to display

---
+++ src/main/java/com/sample/LiquorService.java
@@ -0,0 +1,26 @@
+package com.sample;
+
+import com.sample.model.LiquorType;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class LiquorService {
+
+    public List<String> getAvailableBrands(LiquorType type) {
+        List<String> brands = new ArrayList<String>();
+        if(type.equals(LiquorType.WINE)){
+            brands.add("Orin Swift");
+            brands.add("Caymus Vineyards");
+        } else if (type.equals(LiquorType.WHISKEY)){
+            brands.add("Rebecca Creek");
+            brands.add("Bookers");
+        } else if (type.equals(LiquorType.BEER)){
+            brands.add("Freetail");
+            brands.add("Ballast Point");
+        } else {
+            brands.add("No Brand Available");
+        }
+        return brands;
+    }
+}
