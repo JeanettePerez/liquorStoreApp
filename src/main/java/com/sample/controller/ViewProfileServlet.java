77ac475e3b3446757e7e504ea5388520dda0b981
Jeanette Perez <jeanette.marie.perez@gmail.com>
2021-04-14 19:02:22 -0500

    viewing profile with validation

---
+++ src/main/java/com/sample/controller/ViewProfileServlet.java
@@ -0,0 +1,22 @@
+package com.sample.controller;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+
+@WebServlet("/profile")
+public class ViewProfileServlet extends HttpServlet {
+  @Override
+  protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    HttpSession session = req.getSession();
+     if(session.getAttribute("user") == null){
+       resp.sendRedirect("/login");
+       return;
+     }
+       req.getRequestDispatcher("/WEB-INF/profile.jsp").forward(req,resp);
+  }
+}
