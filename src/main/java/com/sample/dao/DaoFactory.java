b9f8445ba4d5cb08adf349b7fa75dfe3e133ccbf
Jeanette Perez <jeanette.marie.perez@gmail.com>
2021-04-18 16:58:59 -0500

    start of decoupling code

--- src/main/java/com/sample/dao/DaoFactory.java
+++ src/main/java/com/sample/dao/DaoFactory.java
@@ -1,4 +1,12 @@
 package com.sample.dao;
 
 public class DaoFactory {
+  private static Config config = new Config();
+  private static InventoryItems itemsDao;
+  public static InventoryItems getItemsDao() {
+    if(itemsDao == null) {
+      itemsDao = new MySQLBrandDao(config);
+    }
+    return itemsDao;
+  }
 }
